efe3627b6be68e48eb04241e6a9c66b2c8e53c38
Tsigie <tktsigie@yahoo.com>
2018-08-04 19:28:21 -0400

    Created config file

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,9 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItem = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
+// Set Initial State Of Menu
 let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 
